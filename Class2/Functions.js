58f4090fa5117f4d1e6c80487a0514518ef29ab1
anjaliofficial <bistaanjali1415@gmail.com>
2025-11-03 09:39:55 +0545

    ds

--- Class2/Functions.js
+++ Class2/Functions.js
@@ -16,7 +16,7 @@ console.log(second)
 function greetNormal(){
     console.log("Hello World")
 }
-getreetNormal()
+greetNormal()
 
 
 // 
@@ -34,3 +34,49 @@ const arrowFunc2 = (firstName) => {
 }
 
 console.log(arrowFunc2("Anjali"))
+
+
+
+const add1 = (a, b) => a + b
+const subtract = (a, b) => {
+    a - b
+}
+
+
+
+// practice 
+// make a function with "function " keyword named average1
+// that takes three parameters and returns their average
+//make a same function using arrow function named average2 without scope
+//make the same function using arrow function named average3 with scope 
+
+function average1(num1, num2, num3) {
+    let average=  (num1 +num2+num3) / 3
+    return average
+}
+const average2 = (num1, num2, num3) => (num1 + num2 + num3) / 3  
+const average3 = (num1, num2, num3 )=> {
+    let average = (num1 + num2 + num3) / 3
+    return average
+}
+
+
+
+
+
+
+// higher order functions / Callback functions  c
+const cb1 = (a,b,operation) => {
+    console.log("Starting operatoion")
+    // logics
+    return operation(a,b)
+}
+
+
+let num1 = 10
+let num2 = 5
+let sunFunc = (a,b) =>{
+    let sum = a + b
+    console.log(sum)
+    return sum
+}
