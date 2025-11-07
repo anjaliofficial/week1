298e64942a5b3801c47c438b9a51f0cce864c360
anjaliofficial <bistaanjali1415@gmail.com>
2025-11-07 09:27:50 +0545

    oop

--- class4/OOP.js
+++ class4/OOP.js
@@ -25,3 +25,25 @@ console.log(Person.species); // Human
 person1.info(); // Name: Anjali, Age: 19
 person1.setAge = 20;
 console.log(person1.getAge); // 20
+
+
+// inheritance 
+
+
+class Student extends  Person{
+    constructor (name, age, grade ){
+        super(name,age) // aprent consteructor 
+        this.grade = grade
+    }
+    gradeInfo(){
+        console.log(`Name: ${this.name}, Grade: ${this.grade}`)
+    }
+
+
+
+}
+
+
+const student1 = new Student ("Apala", 10, "A")
+student1.info()
+student1.gradeInfo()
